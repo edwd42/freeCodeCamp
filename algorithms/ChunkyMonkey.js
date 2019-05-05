b87d873dea2eb3190bb19ee8e5e4bdb01206fa0d
edwd42 <edwd42@yahoo.com>
2019-05-05 00:11:35 -0400

    Basic Algorithm Scripting: Chunky Monkey

---
+++ algorithms/ChunkyMonkey.js
@@ -0,0 +1,35 @@
+/**
+ * Basic Algorithm Scripting: Chunky Monkey
+
+ * Write a function that splits an array (first argument) into groups the length of size 
+ * (second argument) and returns them as a two-dimensional array.
+ */
+
+function chunkArrayInGroups(arr, size) {
+    // Break it up.
+    var arr2 = [[]];
+    arr2[0] = arr.slice(0,size);
+    var loops = (arr.length / size);
+    for(let i = 1; i < loops; i++){
+        arr2[i] = arr.slice((size * i), (size * (i + 1)));
+    }
+    return arr2;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+
+/**
+ * chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+ */
